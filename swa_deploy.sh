66b930a2148b97ed93a8eb6408ef2caf8fb32951
Carlos Icaza <icazacarlos@gmail.com>
2023-11-24 20:46:28 -0500

    Create swa_deploy.sh

---
+++ swa_deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo -e "\n# --------------------------------------"
+echo "  [PREVIEW] Deploy Metrics"
+echo "# --------------------------------------"
+echo "  Send deploy metrics to DevOps Central"
+echo -e "# --------------------------------------\n"
+
+echo "##[command]NOOP"
