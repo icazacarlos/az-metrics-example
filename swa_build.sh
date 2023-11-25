8e8398ab5a731c4be0b67634409eb7f3fec07f12
Carlos Icaza <icazacarlos@gmail.com>
2023-11-24 21:41:13 -0500

    Create swa_build.sh

---
+++ swa_build.sh
@@ -0,0 +1,7 @@
+echo -e "\n# --------------------------------------"
+echo "  [PREVIEW] Build Metrics"
+echo "# --------------------------------------"
+echo "  Send build metrics to DevOps Central"
+echo -e "# --------------------------------------\n"
+
+logCommand "NOOP"
